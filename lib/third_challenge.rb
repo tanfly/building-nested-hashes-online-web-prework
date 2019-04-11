15b465c59d0493a6ff518d591b7bf323f14c8f9b
Stella McEwen <mcewen.stella@gmail.com>
2019-04-11 23:00:44 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,8 +4,7 @@ def third_challenge
       :patriarch => {
         :name => "Lord Montague", :age => "53"},
       :matriarch => {
-        #your key/value pairs here
-        },
+        :name => "Lady Montague", :age => "54"},
       :hero => {
         #your key/value pairs here
         },
