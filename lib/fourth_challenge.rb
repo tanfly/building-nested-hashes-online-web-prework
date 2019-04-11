2222829cd1b535239a4eb2babfe19d50ef5557a9
Stella McEwen <mcewen.stella@gmail.com>
2019-04-11 23:06:15 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,9 +4,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [ benvolio = {:name => "Benvolio", :age => "17", :attitude => "worried"}, mercutio = {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
-        #your hashes here!
-      ]
+      :hero_friends => [ benvolio = {:name => "Benvolio", :age => "17", :attitude => "worried"}, mercutio = {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}]
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
